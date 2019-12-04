4b3d6054d7088e45a90ef562a225727179948df4
Amakhin <amakhinruslan@gmail.com>
2019-12-04 17:05:16 +0200

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Amakhin Ruslan on 03.12.2019.
+//  Copyright © 2019 Amakhin Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+ 
+    
+}
