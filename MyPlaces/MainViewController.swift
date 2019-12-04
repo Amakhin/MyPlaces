665014091ac9375d43b9380d8cce48e58d06b0ac
Amakhin <amakhinruslan@gmail.com>
2019-12-04 18:28:46 +0200

    New Place ViewController

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -48,6 +48,9 @@ class MainViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     
+    @IBAction func cancelAction(_ segue: UIStoryboardSegue) {
+        
+    }
 
 }
 
